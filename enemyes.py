f5155cbddcf2acf16214db441e2121b9d8304b3b
its-times <nikitaporodko200729@gmail.com>
2025-01-23 23:05:55 +0500

    Прописал поведение врагам и боссам, дал возможность стрелять последним Написал систему рандомной генерации врагов в бесконечном режиме, исходя из количества убийств Сделал косметические доработки

--- enemyes.py
+++ enemyes.py
@@ -1,8 +1,13 @@
 import pygame
 import random
+import json
+with open('settings.json') as f:
+    settings = json.load(f)
+HEALTH_PLAYER = settings['health']
 
-SCREEN_WIDTH = 1300
-SCREEN_HEIGHT = 1000
+from config import *
+
+bullets_boss = pygame.sprite.Group()
 
 class Enemy1(pygame.sprite.Sprite):
     def __init__(self):
@@ -10,6 +15,9 @@ class Enemy1(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/enemy1.png')
         self.rect = self.image.get_rect(center=(random.randint(20, SCREEN_WIDTH - 250), 0))
         self.speed = 2
+        self.damage_crash = HEALTH_PLAYER // 6
+        self.reward = 20
+        self.health = 20
 
     def update(self):
         self.rect.y += self.speed
@@ -22,11 +30,21 @@ class Enemy2(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/enemy2.png')
         self.rect = self.image.get_rect(center=(random.randint(20, SCREEN_WIDTH - 250), 0))
         self.speed = 2.3
+        self.damage_crash = HEALTH_PLAYER // 4
+        self.reward = 30
+        self.health = 40
 
     def update(self):
         self.rect.y += self.speed
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
+        if random.randint(1, 15) == 2:
+            rand = random.randint(1, 2)
+            if rand == 1 and self.rect.x - self.speed * 2.5 >= 0:
+                self.rect.x -= self.speed * 2.5
+            elif rand == 2 and self.rect.x + self.rect.width + self.speed * 3 <= SCREEN_WIDTH:
+                self.rect.x += self.speed * 2.5
+
 
 
 class Enemy3(pygame.sprite.Sprite):
@@ -35,24 +53,54 @@ class Enemy3(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/enemy3.png')
         self.rect = self.image.get_rect(center=(random.randint(0, SCREEN_WIDTH), 0))
         self.speed = 2.5
+        self.damage_crash = HEALTH_PLAYER // 2
+        self.reward = 40
+        self.health = 60
 
     def update(self):
         self.rect.y += self.speed
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
+        rand = random.randint(1, 2)
+        if rand == 1 and self.rect.x - self.speed * 3 >= 0:
+            self.rect.x -= self.speed * 3
+        elif rand == 2 and self.rect.x + self.rect.width + self.speed * 5 < SCREEN_WIDTH:
+            self.rect.x += self.speed * 3
 
-# Дать возможность стрелять для Боссов и переработать их движение(чтобы двигались по горизонтали и меньше по вертикали)
+
 class Boss1(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('data/boss1.png')
         self.rect = self.image.get_rect(center=(random.randint(20, SCREEN_WIDTH - 250), 0))
         self.speed = 3
+        self.bullet_speed = 2
+        self.flag = False
+        self.damage_crash = HEALTH_PLAYER
+        self.damage_bullet = 20
+        self.reward = 100
+        self.health = 100
 
     def update(self):
-        self.rect.y += self.speed
+        global bullets_boss
+        self.rect.y += self.speed * 0.2
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
+        if not self.flag:
+            if self.rect.x - self.speed >= 0:
+                self.rect.x -= self.speed
+            else:
+                self.flag = True
+        else:
+            if self.rect.x + self.rect.width + self.speed <= SCREEN_WIDTH:
+                self.rect.x += self.speed
+            else:
+                self.flag = False
+        rand = random.randint(1, 100)
+        if rand == 5:
+            bullet = BulletBoss(self.rect.centerx, self.rect.bottom, self.bullet_speed, self.damage_bullet)
+            bullets_boss.add(bullet)
+
 
 
 class Boss2(pygame.sprite.Sprite):
@@ -61,11 +109,32 @@ class Boss2(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/boss2.png')
         self.rect = self.image.get_rect(center=(random.randint(20, SCREEN_WIDTH - 250), 0))
         self.speed = 3.3
+        self.bullet_speed = 4
+        self.flag = False
+        self.damage_crash = HEALTH_PLAYER
+        self.damage_bullet = 30
+        self.reward = 150
+        self.health = 140
 
     def update(self):
-        self.rect.y += self.speed
+        global bullets_boss
+        self.rect.y += self.speed * 0.4
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
+        if not self.flag:
+            if self.rect.x - self.speed >= 0:
+                self.rect.x -= self.speed
+            else:
+                self.flag = True
+        else:
+            if self.rect.x + self.rect.width + self.speed <= SCREEN_WIDTH:
+                self.rect.x += self.speed
+            else:
+                self.flag = False
+        rand = random.randint(1, 100)
+        if rand == 5:
+            bullet = BulletBoss(self.rect.centerx, self.rect.bottom, self.bullet_speed, self.damage_bullet)
+            bullets_boss.add(bullet)
 
 
 class Boss3(pygame.sprite.Sprite):
@@ -74,8 +143,43 @@ class Boss3(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/boss3.png')
         self.rect = self.image.get_rect(center=(random.randint(20, SCREEN_WIDTH - 250), 0))
         self.speed = 3.5
+        self.bullet_speed = 6
+        self.flag = False
+        self.damage_crash = HEALTH_PLAYER
+        self.damage_bullet = 40
+        self.reward = 200
+        self.health = 180
 
     def update(self):
-        self.rect.y += self.speed
+        global bullets_boss
+        self.rect.y += self.speed * 0.5
         if self.rect.top > SCREEN_HEIGHT:
+            self.kill()
+        if not self.flag:
+            if self.rect.x - self.speed >= 0:
+                self.rect.x -= self.speed
+            else:
+                self.flag = True
+        else:
+            if self.rect.x + self.rect.width + self.speed <= SCREEN_WIDTH:
+                self.rect.x += self.speed
+            else:
+                self.flag = False
+        rand = random.randint(1, 100)
+        if rand == 5:
+            bullet = BulletBoss(self.rect.centerx, self.rect.bottom, self.bullet_speed, self.damage_bullet)
+            bullets_boss.add(bullet)
+
+
+class BulletBoss(pygame.sprite.Sprite):
+    def __init__(self, x, y, bullet_speed, damage):
+        super().__init__()
+        self.image = pygame.image.load('data/bullet_enemy.png').convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+        self.bullet_speed = bullet_speed
+        self.damage = damage
+
+    def update(self):
+        self.rect.y += self.bullet_speed
+        if self.rect.bottom > SCREEN_HEIGHT:
             self.kill()
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import json
+import random
+from pydoc import visiblename
 
 import pygame
 
+import endless_game
 from enemyes import *
+from config import *
 
 pygame.init()
 
 with open('settings.json') as f:
     settings = json.load(f)
 
-SCREEN_WIDTH = 1100
-SCREEN_HEIGHT = 1000
 PLAYER_HEALTH = settings['health']
 PLAYER_SPEED = settings['speed']
 BULLET_SPEED = settings['bullet_speed']
 BULLET_DAMAGE = settings['bullet_damage']
 BULLET_COL = settings['bullet_col']
 
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Wars")
+game_over_music = pygame.mixer.Sound('data/game_over.mp3')
+shot_music = pygame.mixer.Sound('data/shot.mp3')
+crash_music = pygame.mixer.Sound('data/crash.mp3')
+victrel_music = pygame.mixer.Sound('data/vistrel.mp3')
 
 # Дописать систему выхода с экрана(отнимается хп)
 # Сделать стартовый экран с началом игры(выбор режима), магазином прокачки и таблицей лидеров
@@ -52,6 +59,8 @@ class Bullet(pygame.sprite.Sprite):
 
 
 def main():
+    fon = pygame.image.load('data/fon.png')
+    fon = pygame.transform.scale(fon, (SCREEN_WIDTH, SCREEN_HEIGHT)).convert()
     clock = pygame.time.Clock()
     player = Player()
 
@@ -63,49 +72,83 @@ def main():
 
     score = 0
     hp = PLAYER_HEALTH
-
+    kills = 0
+    conf_spawn = [[0, 70], [70, 95], [95, 100]]
     running = True
+    boss = False
+    lvl_boss = 1
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                settings['cash'] += score
+                with open('settings.json', mode='w') as f:
+                    json.dump(settings, f)
                 running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 if len(bullets) < BULLET_COL:
                     bullet = Bullet(player.rect.centerx, player.rect.top)
                     bullets.add(bullet)
-                    all_sprites.add(bullet )
-
+                    all_sprites.add(bullet)
+                    victrel_music. play()
+        all_sprites.add(bullets_boss)
         all_sprites.update()
 
-        if random.randint(1, 30) == 1: # Заменить на систему выбора врагов исходя из уровня сложности/количества очков
-            enemy = Enemy1()
-            enemies.add(enemy)
-            all_sprites.add(enemy)
+        conf_spawn = [[conf_spawn[0][0], int(round(conf_spawn[0][1] * (1 - (kills / 1000)), 0))],
+                      [int(round(conf_spawn[1][0] * (1 - (kills / 1000)), 0)),
+                      int(round(conf_spawn[1][1] * (1 - (kills / 1000)), 0))],
+                      [int(round(conf_spawn[2][0] * (1 - (kills / 1000)), 0)),
+                       int(round(conf_spawn[2][1] * (1 - (kills / 1000)), 0))]]
 
-        hits = pygame.sprite.groupcollide(bullets, enemies, True, True)
-        score += len(hits) * 15
+        chance_boss = kills / 50
+        if chance_boss >= 100:
+            if lvl_boss < 3:
+                lvl_boss += 1
+                chance_boss = 0
+            else:
+                chance_boss = chance_boss / 4
+        kills_mejdu_boss = 0
+        enemies, all_sprites, boss, kills_mejdu_boss = endless_game.generate_enemy(kills, lvl_boss, boss, enemies, all_sprites, chance_boss, kills_mejdu_boss)
+
+        hits = pygame.sprite.groupcollide(bullets, enemies, True, False )
+        for bullet, hit in hits.items():
+            hit[0].health -= BULLET_DAMAGE
+            if hit[0].health <= 0:
+                if hit[0].__class__.__name__ in ['Boss1', 'Boss2', 'Boss3']:
+                    boss = False
+                score += hit[0].reward
+                hit[0].kill()
+                kills += 1
+                crash_music.play()
+                kills_mejdu_boss += 1
+            else:
+                shot_music.play()
 
         crash_in_player = pygame.sprite.spritecollide(player, enemies, True)
-        if len(crash_in_player) * 20 < hp:
-            hp -= len(crash_in_player) * 20
-        else:
-            hp = 0
+        for crash in crash_in_player:
+            hp -= crash.damage_crash
+        if hp <= 0:
             running = False
 
+        hits_boos_in_player = pygame.sprite.spritecollide(player, bullets_boss, True)
+        for bul in hits_boos_in_player:
+            hp -= bul.damage
+        if hp <= 0:
+            running = False
+            crash_music.play()
 
 
         bullets.update()
-
         screen.fill((0, 0, 0))
+        screen.blit(fon, (0, 0))
         all_sprites.draw(screen)
 
         pygame.display.flip()
         clock.tick(60)
 
-
     settings['cash'] += score
     with open('settings.json', mode='w') as f:
         json.dump(settings, f)
+
     pygame.quit()
 
 
